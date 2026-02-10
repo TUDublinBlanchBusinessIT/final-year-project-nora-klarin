97e3192c5440bc46493c906471b5076e2009232a
nora-klarin <b00177535@mytudublin.ie>
2026-02-10 12:05:30 +0000

    Add carer messaging feature

---
+++ app/Models/Message.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class Message extends Model
+{
+    protected $fillable = ['sender_id', 'recipient_id', 'body', 'read_at'];
+
+    public function sender()
+    {
+        return $this->belongsTo(User::class, 'sender_id');
+    }
+
+    public function recipient()
+    {
+        return $this->belongsTo(User::class, 'recipient_id');
+    }
+}
