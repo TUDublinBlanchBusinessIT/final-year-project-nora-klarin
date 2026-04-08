c963d2162c41e39bb71a07937a219ee101261726
Samira Ahmed <B00156786@mytudublin.ie>
2026-04-08 19:09:45 +0100

    fixed issues

--- app/Http/Controllers/CarerCaseFileController.php
+++ app/Http/Controllers/CarerCaseFileController.php
@@ -1,42 +1,83 @@
 <?php
 
+
+
 namespace App\Http\Controllers;
 
+
+
 use Illuminate\Http\Request;
+
 use App\Models\CaseFile;
 
+
+
 class CarerCaseFileController extends Controller
+
 {
+
     public function show(Request $request, $case)
+
     {
+
         $user = $request->user();
 
+
+
         if (($user->role ?? null) !== 'carer') {
+
             abort(403, 'Unauthorized');
+
         }
 
+
+
         $case = CaseFile::with([
+
             'youngPerson',
+
             'carers',
-            'placements.placement.carer',
+
+            'placements.carer', // ✅ FIXED HERE
+
             'medicalInfos',
+
             'educationInfos',
+
             'documents',
+
             'appointments.creator',
+
             'appointments.carers',
+
             'wellbeingChecks.submittedBy',
+
         ])
+
         ->where(function ($query) use ($case) {
+
             $query->where('id', $case)
+
                   ->orWhere('case_reference', $case);
+
         })
+
         ->whereHas('carers', function ($query) use ($user) {
+
             $query->where('users.id', $user->id);
+
         })
+
         ->firstOrFail();
 
+
+
         return view('carer.case-file.show', [
+
             'case' => $case,
+
         ]);
+
     }
+
 }
